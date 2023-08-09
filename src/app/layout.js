ac4ce628887e431c3c6b19ebde276ec8fa5a8ab5
fluk3G123 <nitipong4434@gmail.com>
2023-08-09 15:33:32 +0700

    ok mak

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Nithipong Howong"
+            studentId="650612087"
           />
         </div>
       </body>
